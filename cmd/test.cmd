8525f2acdbcf5671b1dc244c111979bf7966f601
hjtsaijoe <38649636+hjtsaijoe@users.noreply.github.com>
2020-03-11 00:44:20 +0800

    add test.cmd
    add cmd/ folder for *.cmd files

---
+++ cmd/test.cmd
@@ -0,0 +1,7 @@
+echo     Example how to start marking bouded boxes for training set Yolo v2
+
+
+D:\C_projects\Yolo_mark\x64\Release\yolo_mark.exe D:\C_projects\darknet_preprocessing\test D:\C_projects\darknet_preprocessing\test_list\total.txt D:\C_projects\darknet_preprocessing\test_list\obj.names
+
+
+pause
