f7343ae8ccab488a887427ef7e6635237016d55b
hjtsaijoe <38649636+hjtsaijoe@users.noreply.github.com>
2020-04-02 15:45:48 +0800

    add data_training.cmd

---
+++ cmd/data_training.cmd
@@ -0,0 +1,7 @@
+echo     Example how to start marking bouded boxes for training set Yolo v2
+
+
+D:\C_projects\Yolo_mark\x64\Release\yolo_mark.exe D:\datasets\NTUH_dataset\data_training D:\datasets\NTUH_dataset\darknet_lists\data_training.txt D:\datasets\NTUH_dataset\darknet_configurations\names\obj.names
+
+
+pause
